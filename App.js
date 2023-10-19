cd42e5bd0e8d54a644d9a6142c89eb928530e1d4
Hitanshi-Singh <tech.hitanshi@gmail.com>
2023-10-20 00:08:13 +0530

    the start of the food delivery app

--- App.js
+++ App.js
@@ -1,18 +1,43 @@
-/*ReactElement(object)=>HTML(browser understands)*/
 import React from "react";
 import ReactDOM from "react-dom/client";
-const Title = () => (
-  <h2 className="head" tabIndex={5}>
-    Welcome to Namaste React🚀
-  </h2>
-);
-const Headingcomponent = () => (
-  <div className="container">
-    {Title()}
-    <Title />
-    <Title></Title>
-    <h1 className="heading">hello from the functional component</h1>
-  </div>
-);
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          src="https://png.pngtree.com/png-clipart/20230106/original/pngtree-simple-and-modern-food-logo-vector-design-png-image_8876455.png"
+          alt="logo"
+          className="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search"></div>
+      <div className="res-container"></div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Headingcomponent />);
+root.render(<AppLayout />);
